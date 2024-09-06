41ef65fee4504a3f9730aa55f410df70f31169da
Amirni19 <amir19nigmatullin.19@gmail.com>
2024-09-06 20:22:41 +0300

    practis2

---
+++ java/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// let admin = "Джон";
+// let name = "Джон";
+// // alert(admin);
+
+// let city = prompt('Введите названия города', );
+// let start = prompt('Введите начало очнование',);
+// let popul = prompt('Введите популяцию ');
+// let year = 2024;
+// alert('Городу ' + city + ' исполнилось ' + (year-start) + ' лет с момента его образования. Население ' + popul + 'человек');
+
+// let pi = 3.14;
+// let R = prompt('Введите радиус',);
+// alert(pi * (R ** 2));
+
+// let a = prompt('Введите первое число',);
+// let b = prompt('Введите второе число');
+// let sum = a + b;
+// let raz = a - b;
+// let chas = a / b;
+// let proiz = a * b;
+// alert('Сумма ' + sum + ' Разность ' + raz + ' Частное ' + chas + ' Произведение ' + proiz);
